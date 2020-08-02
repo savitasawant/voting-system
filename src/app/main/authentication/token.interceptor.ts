cfd8985c85a078239a93df4273723e03b22f7f00
savita <savita.sawant@freightbro.com>
2020-08-02 16:16:33 +0530

    auth

--- src/app/main/authentication/token.interceptor.ts
+++ src/app/main/authentication/token.interceptor.ts
@@ -22,7 +22,7 @@ export class TokenInterceptor implements HttpInterceptor {
 
     request = request.clone({
       setHeaders: {
-        Authorization: `token ${this.auth.authToken}`
+        // Authorization: `token ${this.auth.authToken}`
         // 'referer-domain': hostName
       }
     });
