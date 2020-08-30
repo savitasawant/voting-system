82d9807dbdf7f9dd64c96a4576c36542b8a8f716
savita <savita.sawant@freightbro.com>
2020-08-30 15:01:42 +0530

    development config

--- src/global.variables.ts
+++ src/global.variables.ts
@@ -1,6 +1,6 @@
-// export const site = 'http://localhost:4200/';
-export const site = 'http://18.191.19.203/';
+export const site = 'http://localhost:4200/';
+// export const site = 'http://18.191.19.203/';
 
-// export const url = 'http://localhost:8075/';
-export const url = 'http://18.191.19.203/';
-export const localHostName = '3.129.12.90';
+export const url = 'http://localhost:8075/';
+// export const url = 'http://18.191.19.203/';
+export const localHostName = '18.191.19.203';
